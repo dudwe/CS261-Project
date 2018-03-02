d974fd24e0ecd4f67206d5425c3825c16fd5aca8
Bhavin Shah <bhavin200@hotmail.co.uk>
2018-03-02 16:29:57 +0000

    fixed the rss and the test for it

--- Prototypes/P1/ParsingAndProcessing/rss.php
+++ Prototypes/P1/ParsingAndProcessing/rss.php
@@ -4,31 +4,10 @@ ini_set('display_errors', '1');
     include_once('../Database/interface.php');
     include_once('../Database/SentimentAnalysis/analyse_sentiment.php');
 	//var_dump(getRSS("Fixed Line Telecommunications",True,$conn));
-	
-	
-    function get_sectors($conn){ // Create sectors list
-   		$sql = "SELECT sector_name FROM sectors";
 
-	    $result = $conn->query($sql);
 
-	    $sectorNames= array();
-
-	    if ($conn->query($sql)) {
-	    	while($row = $result->fetch_assoc())
-	        {
-	            array_push($sectorNames, $row["sector_name"]);
-	        }
-
-	    	$result->close();
-
-	    	return $sectorNames;
-	    } else {
-	        echo "Error retrieving sectors names: " . $conn->error . "<br>";
-	        return 0;
-	    }
-	}
-
-	function get_tickers($conn, $sectorId){ // Create ticker list
+	function get_tickers($sectorId){ // Create ticker list
+        $conn=db_connection();
    		$sql = "SELECT ticker_symbol FROM stocks WHERE sector_id = ".$sectorId;
 
 	    $result = $conn->query($sql);
@@ -38,8 +17,11 @@ ini_set('display_errors', '1');
 	    if ($conn->query($sql)) {
 	    	while($row = $result->fetch_assoc())
 	        {
-	            if( $row["ticker_symbol"] = "BT.A")	$ticker = "BT.L";
-	            else $ticker= str_replace(".", "", $row["ticker_symbol"]).".L";
+	            if( $row["ticker_symbol"] == "BT.A")	{
+                    $ticker = "BT.L";
+	            }else {
+                    $ticker= str_replace(".", "", $row["ticker_symbol"]).".L";
+	            }
 	            array_push($tickers, $ticker);
 	        }
 
@@ -51,55 +33,72 @@ ini_set('display_errors', '1');
 	        return 0;
 	    }
 	}
+		
+
 	
+	/*$res=getSingleSector("Banks");
+	print_r($res);
+	$tickers=get_tickers($res['sector_id']);
+	print_r($tickers);*/
 
-	//getRSS("BCS",False,$conn);
-	function getRSS($search,$ftse100){
-        $conn= db_connection();
-        $rss = new DOMDocument();
-        $sectorNames= get_sectors($conn); 
-        //$search = 'BCS';
-        //$search = 'Fixed Line Telecommunications'; //receive intent
+    function getSingleSector($search){ // Create sectors list
+        $conn=db_connection();
+   		$sql = "SELECT sector_name,sector_id  FROM sectors WHERE sector_name = '".$search."'";
 
-        //$ftse100 = False; //Else ftse350news
+	    $result = $conn->query($sql);
+        
+        return $res=$result->fetch_assoc();
+	}
+	
+    function scanCSV($search){
+        $file = fopen("ftse350.csv","r");
 
-        if(in_array($search, $sectorNames)){
-            if($ftse100){
-                $sectorId = array_search($search, $sectorNames)+1;
-
-                $tickerSymbols = get_tickers($conn, $sectorId);
-            }else{
-                $file = fopen("ftse350.csv","r");
-
-                while(! feof($file)){
-                    $csv=fgetcsv($file);
-                    if($search = $csv[0]){
+        while(! feof($file)){
+            $csv=fgetcsv($file);
+                if($search == $csv[0]){
                         if($csv[1] !== ""){
-                                $tickerSymbols = array($csv[1]);
+                                $tickers = array($csv[1]);
                         }
                         for($x = 2; $x < count($csv); $x++){
                             if($csv[$x] !== ""){
-                                array_push($tickerSymbols, $csv[$x]);
+                                array_push($tickers, $csv[$x]);
                             }
                         }
                     }
                 }
-
-                fclose($file);
+        fclose($file);  
+        return $tickers;
+    }
+	
+    
+	function getRSS($search,$ftse100){
+        $conn= db_connection();
+        $rss = new DOMDocument();
+        //get the sector the user asked, if blank this indicates that they aare looking for a stock code
+        $sectordata=getSingleSector($search);
+        if(empty($sectordata)){
+            /*we got a stock id*/
+            if( $search === "BT.A"){	
+                $search = "BT.L";
+            }else{
+                $search=str_replace(".", "", $search).".L";
             }
-
-            $search = '';
-
-            foreach($tickerSymbols as $ticker){
-                if($search !== ''){
-                    $search = $search.','.$ticker;
-                }else{	
-                    $search = $ticker;
-                }
+        }else{
+            /*we got a sector*/
+            if($ftse100){
+                $tickers=get_tickers($sectordata['sector_id']);
+                
+            }else{
+                /*is we are looking at the 350*/
+                //echo "looking at 350";
+                $tickers=scanCSV($search);
             }
-
-            echo $search;
+            //var_dump($tickers);
+            $search = join(', ', $tickers);
+            //echo $tickers;
         }
+        
+
 
         $rss->load('https://feeds.finance.yahoo.com/rss/2.0/headline?s='
                     .$search.
@@ -107,13 +106,13 @@ ini_set('display_errors', '1');
         $feed = array();
         
         foreach ($rss->getElementsByTagName('item') as $node) {
-            if(strpos($node->getElementsByTagName('title')->item(0)->nodeValue, "Form") === false){
+            if(stripos($node->getElementsByTagName('title')->item(0)->nodeValue, "Form") === false){
                 $objOutput = new stdClass();    
                 $objOutput->title=$node->getElementsByTagName('title')->item(0)->nodeValue;
                 $objOutput->desc=$node->getElementsByTagName('description')->item(0)->nodeValue;
                 $objOutput->link=$node->getElementsByTagName('link')->item(0)->nodeValue;
                 $objOutput->date=$node->getElementsByTagName('pubDate')->item(0)->nodeValue;
-                //$objOutput->sentiment=analyse_headline_sentiment($objOutput->title);/*TURN THIS OF FOR TESTSING*/
+                $objOutput->sentiment=analyse_headline_sentiment($objOutput->title);/*TURN THIS OF FOR TESTSING*/
                 array_push($feed, $objOutput);
             }
             
