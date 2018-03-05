3a8934359045f73643b38add8f2d61d249ba5be8
Kyle Gough <kylegough98@gmail.com>
2018-03-05 18:54:47 +0000

    Graph lines and point made clearer

--- Prototypes/P1/UserInterface/js/main.js
+++ Prototypes/P1/UserInterface/js/main.js
@@ -787,7 +787,8 @@ $(document).ready(function() {
           ticks: { beginAtZero: false },
           scaleLabel : { display: true, labelString: "Price" }
         }]},
-        title: { display: true, text: graphTitle}
+        title: { display: true, text: graphTitle},
+        elements: { point: { radius: 0 } }
       }
     });
   }
